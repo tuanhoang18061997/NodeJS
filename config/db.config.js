6dac3ab4cdbe5ea1e2d881e5167f39a719607ed5
DESKTOP-TQJM7DF\DELL <tuanhoang18061997@gmail.com>
2021-04-08 11:00:17 +0700

    8/4/2021

--- config/db.config.js
+++ config/db.config.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'nodejs'
+  database: 'nodejs',
+  multipleStatements: true
 });
 
 connection.connect((err) => {
